4c9fb871ae11ead7967775fccda59733e070d6c3
Hanisah Musrin <s10226112@connect.np.edu.sg>
2024-01-10 23:59:59 +0800

    chore: set up codes for web server and routing

---
+++ website/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'KNO4@33#R3T49124141ararf4'
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    return app
---
+++ website/auth.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return
