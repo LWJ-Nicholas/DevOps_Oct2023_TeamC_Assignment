4736c7c384802e50f669ff725480390d7ac3800f
Hanisah Musrin <s10226112@connect.np.edu.sg>
2024-01-14 19:15:23 +0800

    fix: fix all the merge conflicts to this branch

--- main.py
+++ main.py
@@ -1,26 +1,6 @@
-from flask import Flask, render_template, request
+from website import create_app
 
-app = Flask(__name__)
-
-
-# Route for the form page
-@app.route('/form', methods=['GET', 'POST'])
-def form():
-    if request.method == 'POST':
-        # Access form data
-        name = request.form['name']
-        email = request.form['email']
-        message = request.form['message']
-
-        # Do something with the form data (you can process or store it)
-        # For example, print the data to the console
-        print(f"Name: {name}, Email: {email}, Message: {message}")
-
-        # Return a response (you can redirect or render a template)
-        return render_template('success.html', name=name)  # Render a success page with the submitted name
-
-    return render_template('CreateEntry.html')
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- website/auth.py
+++ website/auth.py
@@ -62,3 +62,21 @@ def createAccount():
 @login_required
 def welcome():
     return render_template("welcome.html",user=current_user)
+
+# Route for the form page
+@auth.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        # Access form data
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        # Do something with the form data (you can process or store it)
+        # For example, print the data to the console
+        print(f"Name: {name}, Email: {email}, Message: {message}")
+
+        # Return a response (you can redirect or render a template)
+        return render_template('success.html', name=name)  # Render a success page with the submitted name
+
+    return render_template('CreateEntry.html')
