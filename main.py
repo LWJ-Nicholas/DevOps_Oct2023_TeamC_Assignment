de04247d65767f0f5ae07416b57291cae125859e
LWJ-Nicholas <S10222425@connect.np.edu.sg>
2024-01-14 18:56:56 +0800

    Updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from website import create_app
 
-app = create_app()
+        # Return a response (you can redirect or render a template)
+        return render_template('success.html', name=name)  # Render a success page with the submitted name
+
+    return render_template('CreateEntry.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
