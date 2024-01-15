9a96d0e983e16bc200da29554aa58c25b25536a4
Sherlena Ngui <66990228+C4RR0T02@users.noreply.github.com>
2024-01-15 23:04:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import pytest
 
 def test_mytest():
     with pytest.raises(SystemExit) as pytest_wrapped_e:
-        SystemExit(1)
+        raise SystemExit(1)
         assert pytest_wrapped_e.type == SystemExit
         assert pytest_wrapped_e.value.code == 42
